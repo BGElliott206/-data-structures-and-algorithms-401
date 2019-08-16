b1df363dc80dc44ccdb465a0b00b2eb833d60787
Bradley Elliott <bgelliott206@gmail.com>
2019-08-15 23:04:54 -0700

    arrayReverse added

--- challenges/arrayReverse/array-reverse.js
+++ challenges/arrayReverse/array-reverse.js
@@ -1,2 +1,13 @@
 'use strict';
 
+let array = [1, 2, 3, 4, 5, 6];
+
+lastValue = arr.length -1 -i;
+
+firstValue = i;
+
+let.temp = arr [i];
+
+arr[i] = arr [arr.length -1 -i];
+
+arr [arr.length -1 -i] = temp;
